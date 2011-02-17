9bdba26a41ff283b966e37835f1109d684343994
Chris Egeland <phuzion@watson.(none)>
2011-02-17 07:31:12 -0500

    Changed URL from CharField to URLField and set it to verify that a URL loads

--- links/models.py
+++ links/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Link(models.Model):
-	url = models.CharField(max_length=255)
+	url = models.URLField(verify_exists=True)
 	name = models.CharField(max_length=255)
 	def __unicode__(self):
 		return self.name
